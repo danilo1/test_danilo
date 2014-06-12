f4912433b218810ed2288982dc86bee50ef2796c
Danilo Jara <d-jara@neiu.edu>
2014-06-11 22:19:28 -0500

    some small changes

--- src/read.py
+++ src/read.py
@@ -1,4 +1,5 @@
 filename = r'essays.txt'
+import string 
 
 numlines = 0
 
@@ -8,5 +9,7 @@ textfile = open(filename, 'r')
 
 for line in textfile:
 	numlines+=1
+	out = line.translate(string.maketrans("",""), string.punctuation)
+	print out
 	
 print numlines
