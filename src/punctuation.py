d6dec9c60cf043f27f0da17c3e49d5117a68fba4
Sadiya Sultana <suvastaris@gmail.com>
2014-06-20 09:55:11 -0500

    made some changes

--- src/punctuation.py
+++ src/punctuation.py
@@ -1,14 +1,11 @@
-#Eliminates the punctuation from a text file
+#Eliminates the punctuation from a text file and print it in a seperate file
+import string
 filename = r'essays.txt'
-import string 
-
-numlines = 0
-
 textfile = open(filename, 'r')
-
-
+file = open('output.txt', 'w')
 
 for line in textfile:
-	out = line.translate(string.maketrans("",""), string.punctuation)
-	print out
+        out = line.translate(string.maketrans("",""), string.punctuation)
+        file.write(out) # put all document in a new file
+file.close()
 	
